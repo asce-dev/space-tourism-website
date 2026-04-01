d3280898cf6160d6f09d153bb894df808c0a63fe
dystal-dev <jeremyhabal@yahoo.com>
2026-04-01 18:44:16 +0800

    chore: scaffold page components and routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Destination from "./components/Destination";
+import Crew from "./components/Crew";
+import Technology from "./components/Technology";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div>welp</div>;
+  return (
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <>
+      <Link to="/crew">Home</Link>
+      <Link to="/destination">Destination</Link>
+      <Link to="/crew">Crew</Link>
+      <Link to="/technology">Technology</Link>
+    </>
+  );
+}
