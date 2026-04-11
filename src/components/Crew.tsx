c36e8a092bb85ca991e5373dd83584153d6e03c0
dystal-dev <jeremyhabal@yahoo.com>
2026-04-11 12:44:25 +0800

    feat: add crew responsiveness and minor tweaks on destionation

--- src/components/Crew.tsx
+++ src/components/Crew.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { crew, type CrewMember } from "../data/data";
 import Button from "./ui/Button";
+import SectionHeader from "./ui/SectionHeader";
 
 export default function Crew() {
   const [currentCrewMember, setCurrentCrewMember] = useState<CrewMember>(
@@ -8,50 +9,53 @@ export default function Crew() {
   );
 
   return (
-    <div className="flex flex-col w-full max-w-[1110px] gap-300 my-600 mx-auto h-full">
-      <div className="flex gap-300">
-        <div className="text-preset-5 text-white/25 uppercase font-bold">
-          02
-        </div>
-        <div className="text-preset-5 uppercase">meet your crew</div>
-      </div>
-      <div className="flex w-full h-full gap-400">
-        <div className="flex flex-col w-full h-full gap-500">
-          <div className="flex flex-col h-full w-full justify-center gap-300">
-            <div className="flex flex-col text-preset-8 uppercase gap-200">
-              <div className="text-preset-4 uppercase text-white/50">
-                {currentCrewMember.role}
+    <div className="flex flex-col flex-1 p-300 md:p-500 lg:py-0 lg:px-800">
+      <div className="flex flex-1 flex-col w-full max-w-[1110px] gap-300 mx-auto lg:my-600">
+        <SectionHeader number={2} sectionTitle={"meet your crew"} />
+        <div className="flex flex-1 flex-col w-full gap-400 lg:flex-row">
+          <div className="flex flex-1 flex-col w-full gap-300 pt-500 items-center md:flex-none lg:flex-1 lg:gap-500 lg:justify-center lg:pt-0 lg:min-w-[445px] lg:items-start">
+            <div className="flex flex-1 flex-col w-full justify-center gap-300 text-center max-w-[640px] md:max-w-[512px] md:flex-none lg:flex-1 lg:max-w-none lg:text-start">
+              <div className="flex flex-col text-preset-8 uppercase gap-100 md:gap-200">
+                <div className="text-preset-4 uppercase text-white/50 md:text-preset-4-tablet lg:text-preset-4-desktop">
+                  {currentCrewMember.role}
+                </div>
+                <div className="text-preset-3 uppercase md:text-preset-3-tablet lg:text-preset-3-desktop">
+                  {currentCrewMember.name}
+                </div>
               </div>
-              <div className="text-preset-3 uppercase">
-                {currentCrewMember.name}
-              </div>
-            </div>
-            <div className="flex flex-col gap-200">
-              <div className="text-preset-9 text-blue-300">
-                {currentCrewMember.bio}
+              <div className="flex flex-col gap-200">
+                <div className="text-preset-9 text-blue-300 md:text-preset-9-tablet lg:text-preset-9-desktop">
+                  {currentCrewMember.bio}
+                </div>
               </div>
             </div>
+            <div className="flex w-full gap-200 justify-center lg:gap-500 lg:mb-600 lg:justify-start">
+              {crew.map((item) => (
+                <Button
+                  key={item.name}
+                  variant="dotIndicator"
+                  className={`${
+                    item.name === currentCrewMember.name ? "!bg-white/100" : ""
+                  }`}
+                  onClick={() => setCurrentCrewMember(item)}
+                ></Button>
+              ))}
+            </div>
           </div>
-          <div className="flex w-full gap-500 mb-600">
-            {crew.map((item) => (
-              <Button
-                key={item.name}
-                variant="dotIndicator"
-                className={`${
-                  item.name === currentCrewMember.name ? "!bg-white/100" : ""
-                }`}
-                onClick={() => setCurrentCrewMember(item)}
-              ></Button>
-            ))}
-          </div>
-        </div>
 
-        <div className="flex w-full h-full items-center justify-center">
-          <img
-            src={currentCrewMember.image}
-            alt={currentCrewMember.name}
-            className="w-full"
-          />
+          <div className="flex flex-1 w-full items-center justify-center lg:flex-none lg:w-fit">
+            <img
+              src={currentCrewMember.image}
+              alt={currentCrewMember.name}
+              className="w-auto h-auto max-h-[340px] object-contain md:max-h-[560px] overflow-hidden lg:max-h-none lg:h-[676px] xl:w-[539px]"
+              style={{
+                WebkitMaskImage:
+                  "linear-gradient(180deg, white 75%, transparent 100%)",
+                maskImage:
+                  "linear-gradient(180deg, white 75%, transparent 100%)",
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -9,61 +9,63 @@ export default function Destination() {
   );
 
   return (
-    <div className="flex flex-1 flex-col p-300 w-full max-w-[1110px] gap-300 mx-auto md:p-500 lg:my-600 lg:p-0">
-      <SectionHeader number={1} sectionTitle={"pick your destination"} />
-      <div className="flex flex-col flex-1 w-full gap-400 lg:flex-row">
-        <div className="flex flex-1 w-full items-center justify-center basis-0">
-          <img
-            src={currentDestination.image}
-            alt={currentDestination.name}
-            className="h-[150px] w-[150px] md:h-[300px] md:w-[300px] lg:h-[480px] lg:w-[480px]"
-          />
-        </div>
-        <div className="flex flex-1 flex-col w-full items-center justify-center basis-0 md:min-w-[445px]">
-          <div className="flex flex-col w-full md:max-w-[514px] lg:max-w-[445px] gap-300 lg:gap-500">
-            <div className="flex justify-center text-preset-8 gap-400 lg:justify-start">
-              {destinations.map((item) => (
-                <Button
-                  key={item.name}
-                  variant="primary"
-                  className={`
-                    h-400 text-blue-300 hover:text-white
-                    ${
-                      item.name === currentDestination.name
-                        ? "!text-white !border-white/100"
-                        : ""
-                    }
-                  `}
-                  onClick={() => setCurrentDestination(item)}
-                >
-                  {item.name}
-                </Button>
-              ))}
-            </div>
-            <div className="flex flex-col text-center gap-200 lg:text-left">
-              <div className="text-preset-2 uppercase md:text-preset-2-tablet">
-                {currentDestination.name}
+    <div className="flex flex-col flex-1 p-300 md:p-500 lg:py-0 lg:px-800">
+      <div className="flex flex-1 flex-col w-full max-w-[1110px] gap-300 mx-auto lg:my-600">
+        <SectionHeader number={1} sectionTitle={"pick your destination"} />
+        <div className="flex flex-col flex-1 w-full gap-400 lg:flex-row">
+          <div className="flex flex-1 w-full items-center justify-center basis-0">
+            <img
+              src={currentDestination.image}
+              alt={currentDestination.name}
+              className="h-[150px] w-[150px] md:h-[300px] md:w-[300px] lg:h-[480px] lg:min-w-[480px]"
+            />
+          </div>
+          <div className="flex flex-1 flex-col w-full items-center justify-center basis-0 md:min-w-[445px]">
+            <div className="flex flex-col w-full md:max-w-[514px] lg:max-w-[445px] gap-300 lg:gap-500">
+              <div className="flex justify-center text-preset-8 gap-400 lg:justify-start">
+                {destinations.map((item) => (
+                  <Button
+                    key={item.name}
+                    variant="primary"
+                    className={`
+                      h-400 text-blue-300 hover:text-white
+                      ${
+                        item.name === currentDestination.name
+                          ? "!text-white !border-white/100"
+                          : ""
+                      }
+                    `}
+                    onClick={() => setCurrentDestination(item)}
+                  >
+                    {item.name}
+                  </Button>
+                ))}
               </div>
-              <div className="flex text-preset-9 text-blue-300 md:text-preset-9-tablet lg:text-preset-9-desktop">
-                {currentDestination.description}
-              </div>
-            </div>
-            <hr className="border-white/25" />
-            <div className="flex flex-col w-full gap-300 uppercase text-center md:flex-row lg:text-left">
-              <div className="flex flex-col gap-150 w-full">
-                <div className="text-preset-7 text-preset-7-desktop text-blue-300">
-                  Avg. Distance
+              <div className="flex flex-col text-center gap-200 lg:text-left">
+                <div className="text-preset-2 uppercase md:text-preset-2-tablet">
+                  {currentDestination.name}
                 </div>
-                <div className="text-preset-6 text-preset-6-desktop">
-                  {currentDestination.distance}
+                <div className="flex text-preset-9 text-blue-300 md:text-preset-9-tablet lg:text-preset-9-desktop">
+                  {currentDestination.description}
                 </div>
               </div>
-              <div className="flex flex-col gap-150 w-full">
-                <div className="text-preset-7 text-blue-300">
-                  EST. Travel Time
+              <hr className="border-white/25" />
+              <div className="flex flex-col w-full gap-300 uppercase text-center md:flex-row lg:text-left">
+                <div className="flex flex-col gap-150 w-full">
+                  <div className="text-preset-7 text-preset-7-desktop text-blue-300">
+                    Avg. Distance
+                  </div>
+                  <div className="text-preset-6 text-preset-6-desktop">
+                    {currentDestination.distance}
+                  </div>
                 </div>
-                <div className="text-preset-6 text-preset-6-desktop">
-                  {currentDestination.travel}
+                <div className="flex flex-col gap-150 w-full">
+                  <div className="text-preset-7 text-blue-300">
+                    EST. Travel Time
+                  </div>
+                  <div className="text-preset-6 text-preset-6-desktop">
+                    {currentDestination.travel}
+                  </div>
                 </div>
               </div>
             </div>
